230c680bf7a019ec0df919c27a53274624fbbfca
Shabanabasra <160415880+Shabanabasra@users.noreply.github.com>
2024-12-16 09:15:10 -0800

    Second update for the project

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Navbar from "./Navbar";  // This is the correct import
-
+import Navbar from "./Navbar";  
 
 const Hero = () => {
     return (
         <div id="hero" className="min-h-screen flex flex-col">
-            {/* Navbar positioned at the top */}
+            
             <Navbar />
 
             {/* Main Hero content */}
@@ -15,13 +14,13 @@ const Hero = () => {
                     <div
                         className="w-80 h-80 rounded-full bg-no-repeat bg-cover bg-center"
                         style={{
-                            backgroundImage: "url('/images/banner_bg.PNG') ",
+                            backgroundImage: "url('/images/banner_bg.png') ",
                         }}
                     ></div>
 
                     {/* Text Content */}
                     <div className="text-[80px] sm:text-[100px] font-bold leading-tight">
-                        <p data-aos="zoom-in-up">I'm</p>
+                        <p data-aos="zoom-in-up">I am</p>
                         <p data-aos="zoom-in-up">Shabana</p>
                         <p data-aos="zoom-in-up">Basra</p>
                     </div>
